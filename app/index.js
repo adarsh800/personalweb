b8a60864b1fa220d78eb08c6836146e815deb207
Adarsh Shukla <78147487+adarsh800@users.noreply.github.com>
2022-01-21 23:23:02 +0530

    Create index.js

---
+++ app/index.js
@@ -0,0 +1,16 @@
+const PORT = process.env.PORT || 8000;
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public','index.html'));
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`connected at http://localhost:${PORT}/`)
+  });
